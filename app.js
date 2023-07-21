f6cf771592c3bc929042e058a477833a1b05e5f3
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-07-21 01:58:51 -0600

    29: inicio del proyecto

---
+++ app.js
@@ -0,0 +1,10 @@
+
+function tablaMultiplicar() {
+    const base = 5
+    console.clear();
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${base} x ${i} = ${5 * i}`);
+    }
+}
+
+tablaMultiplicar();
