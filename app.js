33967ddc4d7243e66bc8d9e3cd9fb3bef977b6d1
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-07-21 16:34:43 -0600

    31: Importar archivos de nuestro proyecto

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-const fs = require('fs');
+const { crearArchivo } = require("./helpers/multiplicar");
 
-function tablaMultiplicar() {
-    const base = 4
-    let salida = '';
-    for (let i = 1; i <= 10; i++) {
-        salida += `${base} x ${i} = ${base * i}\n`;
-    }
-
-    console.log(salida);
-    fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
-        if (err) throw err;
-        console.log(`tabla-${base}.txt creado`);
-    });
-}
-
-tablaMultiplicar();
+console.clear();
+const base = 3
+const onSuccess = (nombreArchivo) => console.log(nombreArchivo, 'creado');
+crearArchivo(base)
+    .then(onSuccess)
+    .catch(console.error);
