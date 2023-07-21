e3b95aba0b4225fc71353ead6c07c0bc963a46f7
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-07-21 02:15:25 -0600

    30: Requerir paquetes - require

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+const fs = require('fs');
 
 function tablaMultiplicar() {
-    const base = 5
-    console.clear();
+    const base = 4
+    let salida = '';
     for (let i = 1; i <= 10; i++) {
-        console.log(`${base} x ${i} = ${5 * i}`);
+        salida += `${base} x ${i} = ${base * i}\n`;
     }
+
+    console.log(salida);
+    fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+        if (err) throw err;
+        console.log(`tabla-${base}.txt creado`);
+    });
 }
 
 tablaMultiplicar();
