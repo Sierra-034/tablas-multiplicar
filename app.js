9601d182ab786180f693094234ace45c3bdc81c0
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-07-22 13:05:36 -0600

    37: Colores de la consola

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+require('colors');
 const argv = require("./config/yargs");
 const { crearArchivo } = require("./helpers/multiplicar");
 
 console.clear();
-console.log(argv);
 const { base, listar } = argv;
-const onSuccess = (nombreArchivo) => console.log(nombreArchivo, 'creado');
+const onSuccess = (nombreArchivo) => console.log(nombreArchivo.italic, 'creado');
 crearArchivo(base, listar)
     .then(onSuccess)
     .catch(console.error);
--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,16 +1,21 @@
 const fs = require('fs');
+const colors = require('colors');
 
 async function crearArchivo(base = 5, listar = false) {
     try {
         let salida = '';
         for (let i = 1; i <= 10; i++)
-            salida += `${base} x ${i} = ${base * i}\n`;
+            salida += `${base} ${'x'.green} ${i} ${'='.green} ${base * i}\n`;
     
-        if (listar)
+        if (listar) {
+            console.log('===================='.green);
+            console.log('    Tabla del:', colors.yellow(base));
+            console.log('===================='.green);
             console.log(salida);
-            
+        }
+
         fs.writeFileSync(`tabla-${base}.txt`, salida);
-        return `tabla-${base}.txt creado`;
+        return `tabla-${base}.txt`;
     } catch (error) {
         throw error;
     }
